23da0a703c5860c5f23b58d69c95fcd5a69b7626
Rohit-Rajpoot <sunnyr184@gmail.com>
2026-06-18 21:59:00 +0530

    Done

---
+++ Spring-Boot-Simple/src/main/java/com/rays/common/DropdownListInt.java
@@ -0,0 +1,10 @@
+package com.rays.common;
+
+public interface DropdownListInt {
+	
+	public String getKey();
+	
+	public String getValue();
+
+
+}
---
+++ Spring-Boot-Simple/src/main/java/com/rays/ctl/RoleCtl.java
@@ -0,0 +1,136 @@
+package com.rays.ctl;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rays.common.BaseCtl;
+import com.rays.common.ORSResponse;
+import com.rays.dto.RoleDTO;
+import com.rays.form.RoleForm;
+import com.rays.service.RoleService;
+
+@RestController
+@RequestMapping(value = "Role")
+public class RoleCtl extends BaseCtl {
+
+	@Autowired
+	RoleService service;
+
+	@PostMapping("save")
+	public ORSResponse save(@RequestBody @Valid RoleForm form, BindingResult bindingResult) {
+
+		ORSResponse res = new ORSResponse();
+
+		res = validate(bindingResult);
+
+		if (res.isSuccess() == false) {
+			return res;
+		}
+
+		RoleDTO dto = (RoleDTO) form.getDto();
+
+		Long id = service.add(dto);
+
+		if (id != null && id > 0) {
+			res.addData(dto);
+			res.addMessage("Role Saved Successfully");
+		} else {
+			res.addMessage("Error in role add");
+		}
+
+		return res;
+
+	}
+
+	@PostMapping("update")
+	public ORSResponse update(@RequestBody @Valid RoleForm form, BindingResult bindingResult) {
+
+		ORSResponse res = new ORSResponse();
+
+		res = validate(bindingResult);
+
+		if (res.isSuccess() == false) {
+			return res;
+		}
+
+		RoleDTO dto = (RoleDTO) form.getDto();
+
+		service.update(dto);
+
+		res.addData(dto);
+		res.addMessage("Role Updated Successfully");
+
+		return res;
+
+	}
+
+	@PostMapping("delete/{ids}")
+	public ORSResponse update(@PathVariable Long[] ids) {
+
+		ORSResponse res = new ORSResponse();
+
+		if (ids != null && ids.length > 0) {
+			for (Long id : ids) {
+				service.delete(id);
+				res.addMessage("Record Deleted Successfully");
+				res.setSuccess(true);
+			}
+		}
+
+		return res;
+
+	}
+
+	@GetMapping("get/{id}")
+	public ORSResponse update(@PathVariable Long id) {
+
+		ORSResponse res = new ORSResponse();
+		RoleDTO dto = new RoleDTO();
+		dto = service.findById(id);
+		if (dto != null) {
+			res.addData(dto);
+			res.setSuccess(true);
+		} else {
+			res.addMessage("Record Not Found");
+		}
+
+		return res;
+
+	}
+	
+	@RequestMapping(value = "/search/{pageNo}", method = { RequestMethod.GET, RequestMethod.POST })
+	public ORSResponse search(@RequestBody RoleForm form, @PathVariable(required = false) int pageNo) {
+
+		int pageSize = 5;
+
+		ORSResponse res = new ORSResponse();
+
+		RoleDTO dto = new RoleDTO();
+
+		dto.setName(form.getName());
+
+		List<RoleDTO> list = service.search(dto, pageNo, pageSize);
+
+		if (list != null) {
+			res.addData(list);
+			res.setSuccess(true);
+		} else {
+			res.addMessage("Record Not Found");
+		}
+
+		return res;
+
+	}
+
+}
